8b09eff321a327f7ed437146784ef1833366ac1e
MinHie <55249566+leminhhieust@users.noreply.github.com>
2020-12-24 17:31:42 +0700

    Done pagination

--- models/courses.model.js
+++ models/courses.model.js
@@ -1,19 +1,12 @@
 const db = require('../utils/db');
 const TBL_CATEGORIES = 'courses';
-
+const config = require('../config/default.json')
 module.exports = {
-  all() {
-    const sql = `
-      select c.*, co.*, u.Name as TeacherName
-      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID
-    `;
-    return db.load(sql);
-  },
-
+  
   allWithNew(){
     const sql = `
-      select c.*, co.*, u.Name as TeacherName
-      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID
+      select c.*, co.*, u.Name as TeacherName, ca.CatName
+      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID join categories ca on ca.CatID = c.CatID
       where Datediff(CURRENT_DATE(), c.DateCreate) <=31
     `;
     return db.load(sql);
@@ -21,8 +14,8 @@ module.exports = {
 
   allWithView(){
     const sql = `
-      select c.*, co.*, u.Name as TeacherName
-      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID
+      select c.*, co.*, u.Name as TeacherName, ca.CatName
+      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID join categories ca on ca.CatID = c.CatID
       order by co.ViewCount DESC
       limit 10
     `;
@@ -31,39 +24,67 @@ module.exports = {
 
   allWithOutstanding(){
     const sql = `
-      select c.*, co.*, u.Name as TeacherName
-      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID
+      select c.*, co.*, u.Name as TeacherName, ca.CatName
+      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID join categories ca on ca.CatID = c.CatID
       order by co.Ratings DESC
       limit 4
     `;
     return db.load(sql);
   },
 
-  allWithWeb(){
+  allWithCat(offset) {
     const sql = `
-    SELECT c.*, co.*, u.Name as TeacherName
-    from courses c join categories ca on c.CatID = ca.CatID join count co on co.CourseID = c.CourseID join users u on u.UserID = c.TeacherID
-    where ca.CatType = 1
+      select c.*, co.*, u.Name as TeacherName, ca.CatName
+      from courses c join count co on c.CourseID = co.CourseID join users u on u.UserID = c.TeacherID join categories ca on ca.CatID = c.CatID limit ${config.pagination.limit} offset ${offset}
     `;
     return db.load(sql);
   },
 
-  allWithMobile(){
+  async countWithCat() {
+    const rows = await db.load(`select count(*) as total from courses`);
+    return rows[0].total;
+  },
+
+  allWithWeb(offset){
     const sql = `
-    SELECT c.*, co.*, u.Name as TeacherName
+    SELECT c.*, co.*, u.Name as TeacherName, ca.CatName
     from courses c join categories ca on c.CatID = ca.CatID join count co on co.CourseID = c.CourseID join users u on u.UserID = c.TeacherID
-    where ca.CatType = 0
+    where ca.CatType = 1 limit ${config.pagination.limit} offset ${offset}
     `;
     return db.load(sql);
   },
 
-  byCat(categories) {
+  async countWithWeb() {
+    const rows = await db.load(`select count(*) as total from courses c join categories ca on c.CatID = ca.CatID where CatType = 1`);
+    return rows[0].total;
+  },
+
+  allWithMobile(offset){
     const sql = `
-    SELECT c.*, co.*, u.Name as TeacherName
+    SELECT c.*, co.*, u.Name as TeacherName, ca.CatName
     from courses c join categories ca on c.CatID = ca.CatID join count co on co.CourseID = c.CourseID join users u on u.UserID = c.TeacherID
-    where ca.CatName = '${categories}'
-    `
+    where ca.CatType = 0 limit ${config.pagination.limit} offset ${offset}
+    `;
     return db.load(sql);
+  },
+
+  async countWithMobile() {
+    const rows = await db.load(`select count(*) as total from courses c join categories ca on c.CatID = ca.CatID where CatType = 0`);
+    return rows[0].total;
+  },
+
+  byCat(categories, offset) {
+    const sql = `
+    SELECT c.*, co.*, u.Name as TeacherName, ca.CatName
+    from courses c join categories ca on c.CatID = ca.CatID join count co on co.CourseID = c.CourseID join users u on u.UserID = c.TeacherID
+    where ca.CatName = '${categories}' limit ${config.pagination.limit} offset ${offset}
+    `;
+    return db.load(sql);
+  },
+
+  async countWithByCat(categories) {
+    const rows = await db.load(`SELECT count(*) as total from courses c join categories ca on c.CatID = ca.CatID where ca.CatName = '${categories}'`);
+    return rows[0].total;
   },
 
   async single(id) {
--- routes/courses.route.js
+++ routes/courses.route.js
@@ -1,83 +1,167 @@
 const express = require('express');
 const coursesModel = require('../models/courses.model');
-
+const config = require('../config/default.json')
 const router = express.Router();
 
 router.get('/all', async function(req, res){
-  const courses_all = await coursesModel.all();
+  const page = +req.query.page || 1;
+  if(page < 0) page = 1;
+  const offset = (page -1) * config.pagination.limit;
+
+  const [total, courses_all] = await Promise.all([
+    coursesModel.countWithCat(),
+    coursesModel.allWithCat(offset)
+  ])
+
+  const nPages = Math.ceil(total / config.pagination.limit);
+  const page_items = [];
+  for(i = 1; i <= nPages; ++i){
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
+  }
+
+
   res.render('vwCourses/courses', {
     courses: courses_all,
-    empty: courses_all.count === 0
+    empty: courses_all.length === 0,
+    page_items,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    prev_value: page - 1,
+    next_value: page + 1
   });
 })
 
 router.get('/web', async function(req, res){
-  const courses_web = await coursesModel.allWithWeb();
+  const page = +req.query.page || 1;
+  if(page < 0) page = 1;
+  const offset = (page -1) * config.pagination.limit;
+
+  const [total, courses_all] = await Promise.all([
+    coursesModel.countWithWeb(),
+    coursesModel.allWithWeb(offset)
+  ])
+
+  const nPages = Math.ceil(total / config.pagination.limit);
+  const page_items = [];
+  for(i = 1; i <= nPages; ++i){
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
+  }
+
+
   res.render('vwCourses/courses', {
-    courses: courses_web,
-    empty: courses_web.count === 0
+    courses: courses_all,
+    empty: courses_all.length === 0,
+    page_items,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    prev_value: page - 1,
+    next_value: page + 1
   });
 })
 
 router.get('/web/:categories', async function (req, res) {
   const categories = req.params.categories;
-  const courses_byCat = await coursesModel.byCat(categories);
-  if (courses_byCat === null) {
-    return res.redirect('/');
+  const page = +req.query.page || 1;
+  if(page < 0) page = 1;
+  const offset = (page -1) * config.pagination.limit;
+
+  const [total, courses_all] = await Promise.all([
+    coursesModel.countWithByCat(categories),
+    coursesModel.byCat(categories,offset)
+  ])
+
+  const nPages = Math.ceil(total / config.pagination.limit);
+  const page_items = [];
+  for(i = 1; i <= nPages; ++i){
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
   }
 
   res.render('vwCourses/courses', {
-    courses: courses_byCat,
-    empty: courses_byCat.count === 0
+    courses: courses_all,
+    empty: courses_all.length === 0,
+    page_items,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    prev_value: page - 1,
+    next_value: page + 1
   });
 })
 
 router.get('/mobile', async function(req, res){
-  const courses_mobile = await coursesModel.allWithMobile();
+  const page = +req.query.page || 1;
+  if(page < 0) page = 1;
+  const offset = (page -1) * config.pagination.limit;
+
+  const [total, courses_all] = await Promise.all([
+    coursesModel.countWithMobile(),
+    coursesModel.allWithMobile(offset)
+  ])
+
+  const nPages = Math.ceil(total / config.pagination.limit);
+  const page_items = [];
+  for(i = 1; i <= nPages; ++i){
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
+  }
+
+
   res.render('vwCourses/courses', {
-    courses: courses_mobile,
-    empty: courses_mobile.count === 0
+    courses: courses_all,
+    empty: courses_all.length === 0,
+    page_items,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    prev_value: page - 1,
+    next_value: page + 1
   });
 })
 
 router.get('/mobile/:categories', async function (req, res) {
   const categories = req.params.categories;
-  const courses_byCat = await coursesModel.byCat(categories);
-  if (courses_byCat === null) {
-    return res.redirect('/');
+  const page = +req.query.page || 1;
+  if(page < 0) page = 1;
+  const offset = (page -1) * config.pagination.limit;
+
+  const [total, courses_all] = await Promise.all([
+    coursesModel.countWithByCat(categories),
+    coursesModel.byCat(categories,offset)
+  ])
+
+  const nPages = Math.ceil(total / config.pagination.limit);
+  const page_items = [];
+  for(i = 1; i <= nPages; ++i){
+    const item = {
+      value: i,
+      isActive: i === page
+    }
+    page_items.push(item);
   }
 
+
   res.render('vwCourses/courses', {
-    courses: courses_byCat,
-    empty: courses_byCat.count === 0
+    courses: courses_all,
+    empty: courses_all.length === 0,
+    page_items,
+    can_go_prev: page > 1,
+    can_go_next: page < nPages,
+    prev_value: page - 1,
+    next_value: page + 1
   });
 })
-// router.get('/byCat/:id', async function (req, res) {
-//   const catId = req.params.id;
-
-//   for (const c of res.locals.lcCategories) {
-//     if (c.CatID === +catId) {
-//       c.isActive = true;
-//     }
-//   }
-
-//   const rows = await productModel.byCat(catId);
-//   res.render('vwProducts/byCat', {
-//     products: rows,
-//     empty: rows.length === 0
-//   })
-// })
-
-// router.get('/detail/:id', async function (req, res) {
-//   const id = req.params.id;
-//   const product = await productModel.single(id);
-//   if (product === null) {
-//     return res.redirect('/');
-//   }
-
-//   res.render('vwProducts/detail', {
-//     product: product
-//   })
-// })
 
 module.exports = router;

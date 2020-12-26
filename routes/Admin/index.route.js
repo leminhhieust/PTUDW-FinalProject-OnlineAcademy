ef8f823c905b6137db7c09d8f58dc99ab64f5539
HienTran <tranthanhhien2017.kh@gmail.com>
2020-12-27 03:56:28 +0700

    Login_Admin(50%)
    Login_Admin (50%)

--- routes/Admin/index.route.js
+++ routes/Admin/index.route.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
+const userModel = require('../../models/user.model')
+const bcrypt = require('bcryptjs');
 // GET home page.
 router.get('/', async function(req, res) {
-  res.render('home');
-});
+    if (req.session.isAuth === true) {
+        res.render('vwAdmin/vwAdmin')
+    } else {
+        res.redirect('/admin/login');
+    }
+})
+
+router.get('/login', async function(req, res) {
+    if (req.session.isAuth === true) {
+        res.redirect('/admin');
+    } else {
+        res.render('vwAdmin/login', {
+            layout: false
+        });
+    }
+})
+
+router.post('/login', async function(req, res) {
+    const user = await userModel.singleByUserName(req.body.username);
+    if (user === null) {
+        return res.render('vwAdmin/login', {
+            err_message: 'Invalid username or password.'
+        });
+    }
+
+    const ret = bcrypt.compareSync(req.body.password, user.Password);
+    if (ret === false) {
+        return res.render('vwAdmin/login', {
+            err_message: 'Invalid username or password.'
+        });
+    }
+
+    req.session.isAuth = true;
+    req.session.authUser = user;
+
+    let url = '/admin';
+    res.redirect(url);
+})
+
 
 module.exports = router;
--- utils/db.js
+++ utils/db.js
@@ -2,19 +2,19 @@ const mysql = require('mysql');
 const util = require('util');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '02042000',
-  database: 'qlkh',
-  connectionLimit: 50,
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: '123456',
+    database: 'qlkh',
+    connectionLimit: 50,
 });
 
 const pool_query = util.promisify(pool.query).bind(pool);
 
 module.exports = {
-  load: sql => pool_query(sql),
-  add: (entity, tableName) => pool_query(`insert into ${tableName} set ?`, entity),
-  del: (condition, tableName) => pool_query(`delete from ${tableName} where ?`, condition),
-  patch: (entity, condition, tableName) => pool_query(`update ${tableName} set ? where ?`, [entity, condition])
+    load: sql => pool_query(sql),
+    add: (entity, tableName) => pool_query(`insert into ${tableName} set ?`, entity),
+    del: (condition, tableName) => pool_query(`delete from ${tableName} where ?`, condition),
+    patch: (entity, condition, tableName) => pool_query(`update ${tableName} set ? where ?`, [entity, condition])
 };
